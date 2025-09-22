e4a3e7ab313bfd2c14fd67c4554bd2f5af4ce58e
AymenHa01 <hattabaymen15@gmail.com>
2025-09-22 04:17:41 -0700

    productionFix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://artba-fyc3dkejbpdkgxez.westeurope-01.azurewebsites.net'
+  apiUrl: 'https://artba-fyc3dkejbpdkgxez.westeurope-01.azurewebsites.net',
+  blobUrl: 'https://localandtouist.blob.core.windows.net/karima',
+acountName : 'localandtouist', 
+containerName : 'karima'
 };
