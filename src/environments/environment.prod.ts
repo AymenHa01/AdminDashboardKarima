b8b5463d9e23a34f7fbb630547452eff6ec585d2
AymenHa01 <hattabaymen15@gmail.com>
2025-09-22 04:22:55 -0700

    blobUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,7 @@ export const environment = {
   apiUrl: 'https://artba-fyc3dkejbpdkgxez.westeurope-01.azurewebsites.net',
   blobUrl: 'https://localandtouist.blob.core.windows.net/karima',
 acountName : 'localandtouist', 
-containerName : 'karima'
+containerName : 'karima',
+  blobUrlSaS: 'sp=racwdli&st=2025-09-18T11:44:59Z&se=2025-10-01T19:59:59Z&spr=https&sv=2024-11-04&sr=c&sig=eanzkfpnALeSIW6z2%2BU%2FniZVy1gLIdXJT%2BTccfJUNlk%3D',
+
 };
