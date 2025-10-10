31ab82cc599608c51b7964e377e9261b9ec1b768
AymenHa01 <hattabaymen15@gmail.com>
2025-10-10 08:11:11 -0700

    fix add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: false,
    apiUrl: 'https://artba-fyc3dkejbpdkgxez.westeurope-01.azurewebsites.net',
    //apiUrl: 'http://localhost:8086' , 
-  blobUrlSaS: 'https://localandtouist.blob.core.windows.net/karima?sp=racwdli&st=2025-10-10T14:37:36Z&se=2026-03-04T23:52:36Z&spr=https&sv=2024-11-04&sr=c&sig=Z46%2B0HDdrKDGOgdVf07zQo%2B29tnwOLvzrlh9s4hcyXI%3D',
+  blobUrlSaS: 'sp=racwdli&st=2025-10-10T14:54:51Z&se=2026-01-15T00:09:51Z&spr=https&sv=2024-11-04&sr=c&sig=QeLYUJbtGcc0RPOayx9HYh3Fho1O47I6q4mtkIlb99A%3D',
 blobUrl: 'https://localandtouist.blob.core.windows.net/karima',
 acountName : 'localandtouist', 
 containerName : 'karima'
