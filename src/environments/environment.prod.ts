61358b93e995ebc50699af05d2c7462b0ee45c2c
AymenHa01 <hattabaymen15@gmail.com>
2025-10-10 07:53:46 -0700

    fix add images

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   blobUrl: 'https://localandtouist.blob.core.windows.net/karima',
 acountName : 'localandtouist', 
 containerName : 'karima',
-  blobUrlSaS: 'sp=racwdli&st=2025-09-18T11:44:59Z&se=2025-10-01T19:59:59Z&spr=https&sv=2024-11-04&sr=c&sig=eanzkfpnALeSIW6z2%2BU%2FniZVy1gLIdXJT%2BTccfJUNlk%3D',
+  blobUrlSaS: 'https://localandtouist.blob.core.windows.net/karima?sp=racwdli&st=2025-10-10T14:37:36Z&se=2026-03-04T23:52:36Z&spr=https&sv=2024-11-04&sr=c&sig=Z46%2B0HDdrKDGOgdVf07zQo%2B29tnwOLvzrlh9s4hcyXI%3D',
 
 };
